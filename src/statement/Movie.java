0bfb79cba622860137701cf1ee87de5491806ff5
Cherry_Cheng <cherry_cheng@simplo.com.tw>
2016-08-19 08:53:12 +0800

    Get charge function move to Movie object. (switch function need move to owner object)

--- src/statement/Movie.java
+++ src/statement/Movie.java
@@ -25,4 +25,38 @@ public class Movie {
 	public String get_title() {
 		return _title;
 	}
+	
+	double getCharge(int daysRented) {
+		
+		double result = 0;
+		switch (get_priceCode()) {
+		case Movie.REGULAR:
+			result += 2;
+			if (daysRented > 2) {
+				result += (daysRented - 2) * 1.5;
+			}
+			break;
+		case Movie.NEW_RELEASE:
+			result += daysRented * 3;
+			break;
+		case Movie.CHILDRENS:
+			result += 1.5;
+			if (daysRented > 3) {
+				result += (daysRented - 3) * 1.5;
+			}
+			break;
+		default:
+			break;
+		}
+		return result;
+	}
+	
+	int getFrequentRenterPoints(int daysRented) {
+		int result = 1;//Default.
+		//add bonus for a two day new release rental.
+		if (get_priceCode() == Movie.NEW_RELEASE && daysRented > 1) {
+			result = 2;
+		}
+		return result;
+	}
 }
--- src/statement/Rental.java
+++ src/statement/Rental.java
@@ -19,36 +19,10 @@ public class Rental {
 	}
 	
 	double getCharge() {
-		
-		double result = 0;
-		switch (getMovie().get_priceCode()) {
-		case Movie.REGULAR:
-			result += 2;
-			if (getDaysRented() > 2) {
-				result += (getDaysRented() - 2) * 1.5;
-			}
-			break;
-		case Movie.NEW_RELEASE:
-			result += getDaysRented() * 3;
-			break;
-		case Movie.CHILDRENS:
-			result += 1.5;
-			if (getDaysRented() > 3) {
-				result += (getDaysRented() - 3) * 1.5;
-			}
-			break;
-		default:
-			break;
-		}
-		return result;
+		return _movie.getCharge(_daysRented);
 	}
 	
 	int getFrequentRenterPoints() {
-		int result = 1;//Default.
-		//add bonus for a two day new release rental.
-		if (getMovie().get_priceCode() == Movie.NEW_RELEASE && getDaysRented() > 1) {
-			result = 2;
-		}
-		return result;
+		return _movie.getFrequentRenterPoints(_daysRented);
 	}
 }
